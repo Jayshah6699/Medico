7b4a9e13deff72adfe03f9d2218cae9c64fbe7b1
Andrew Robles <andrewroblesdev@gmail.com>
2020-12-19 03:31:16 -0800

    Add department and shift models

--- hours/models.py
+++ hours/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+
+class Shift(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+
