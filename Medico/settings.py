268d96f2772e21e692cb3a877e8bc86887c311ba
Andrew Robles <andrewroblesdev@gmail.com>
2020-12-19 02:41:20 -0800

    Create hours app and create hello world view

--- Medico/settings.py
+++ Medico/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hours.apps.HoursConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
